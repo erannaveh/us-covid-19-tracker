b21820f3585225d4895a9c50f27eebc97c5ca8ce
Eran Naveh <42821407+erannaveh@users.noreply.github.com>
2020-04-09 01:46:44 -0700

    Add files via upload
    add google analytics

--- app.py
+++ app.py
@@ -11,3 +11,32 @@ print('****** start')
 app = dash.Dash(__name__)
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163254429-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163254429-1');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
